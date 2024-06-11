e130a2f709c6c5655a799c8e92b8505d77034e53
lyeiva86 <ulul10039@gmail.com>
2024-06-11 22:14:07 +0700

    fix: seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Status::factory()->count(3)->create();
-        \App\Models\User::factory()->count(10)->create();
+        // \App\Models\User::factory()->count(10)->create();
         \App\Models\Aduan::factory()->count(20)->create();
         // \App\Models\User::factory(10)->create();
 
@@ -23,5 +26,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'username' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'image' => '',
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
